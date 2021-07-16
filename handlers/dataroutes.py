a1ca17ca57157234dc3864fa4af5c9cdb0bf2ed1
Temtsin Sharon <temtsin.sharon@gmail.com>
2021-07-16 16:26:34 +0300

    hyperlink of OK  and csv saver as new data comes from add template

--- handlers/dataroutes.py
+++ handlers/dataroutes.py
@@ -4,14 +4,20 @@ import pandas as pd
 import pickle
 import seaborn as sb
 
-df = sb.load_dataset('diamonds').head(20)
+df = sb.load_dataset('diamonds')
+df.to_csv('./data/diamonds.csv', index=False)
+
+def init():
+    global df
+    df = pd.read_csv('./data/diamonds.csv')
 
 
 def configure(app):
 
     @app.route('/')
     def rtemplate():
-        return render_template('main.html', data=df)
+        init()
+        return render_template('main.html', data=df.head(20))
 
     @app.route('/details/<int:id>')
     def getdetails(id):
@@ -24,3 +30,21 @@ def configure(app):
     @app.route('/predict')
     def predict():
         return render_template('predict.html')
+
+    @app.route('/additem', methods=['POST'])
+    def additem():
+        global df
+        carat = float(request.form['carat'])
+        cut = request.form['cut']
+        color = request.form['color']
+        clarity = request.form['clarity']
+        depth = float(request.form['depth'])
+        table = float(request.form['table'])
+        x = float(request.form['x'])
+        y = float(request.form['y'])
+        z = float(request.form['z'])
+        price = float(request.form['price'])
+        df.loc[df.index.size] = [carat, cut, color,
+                                 clarity, depth, table, price, x, y, z]
+        df.to_csv('./data/diamonds.csv',index=False)
+        return render_template('ok.html')
