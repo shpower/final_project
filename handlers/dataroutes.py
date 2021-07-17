72a38d9d48d6f536e770ca9a3f1306e3ad4d6995
Temtsin Sharon <temtsin.sharon@gmail.com>
2021-07-17 13:45:07 +0300

    predict implementation + result template and go to list while press Ok

--- handlers/dataroutes.py
+++ handlers/dataroutes.py
@@ -1,5 +1,5 @@
 from flask import request, render_template
-import json
+import numpy as np
 import pandas as pd
 import pickle
 import seaborn as sb
@@ -15,6 +15,56 @@ def init():
 def show_alert():
     alert = "Prediction page - Please enter valid input in the page fields"
     return render_template("primary_alert.html", data=alert)
+
+def show_model_alert(alert):
+    return render_template("model_alert.html", data=alert)
+
+def load_model(filename):
+
+    with open('./models/' + filename, 'rb') as model:
+        return pickle.load(model)
+
+def get_model(input):
+    carat = input[0]
+    x = input[6]
+    y = input[7]
+    z = input[8]
+    color_dict = {'D':1, 'E':2, 'F':3, 'G':4, 'H':5, 'I':6, 'J':7}
+    clarity_dict = {'IF':1, 'VVS1':2, 'VVS2':3, 'VS1':4, 'VS2':5, 'SI1':6, 'SI2':7, 'I1':8}
+    cut_dict = {'Ideal':1, 'Premium':2, 'Very Good':3, 'Good':4, 'Fair':5}
+    input[1] = cut_dict[input[1]]
+    input[2] = color_dict[input[2]]
+    input[3] = clarity_dict[input[3]]
+
+    volume = x*y*z
+    if carat <= 0.5:
+        return load_model('df31model.sav')
+
+    elif 0.6 >= carat > 0.5:
+        return load_model('df32model.sav')
+
+    elif 1.2 >= carat > 0.6:
+        if x<6:
+            return load_model('df331model.sav')
+        else:
+            return load_model('df332model.sav')
+
+    elif 1.2 >= carat > 0.6:
+        if x<6:
+            return load_model('df331model.sav')
+        else:
+            return load_model('df332model.sav')
+
+        
+    elif carat > 1.2 and volume < 300:
+        if volume < 225:
+            return load_model('df341model.sav')
+        else:
+            return load_model('df342model.sav')
+
+    else:
+        return "not enough data to predict the price of this diamond" 
+
 
 def configure(app):
 
@@ -72,7 +122,16 @@ def configure(app):
         if "Select" in color or "Select" in clarity or "Select" in cut:
             return show_alert()
 
-        v = dfmodel.predict([carat, cut, color, clarity, depth, table, 0, x, y, z])
-        return render_template('res.html',val=v)
+        input = [carat, cut, color, clarity, depth, table, x, y, z]
+        model = get_model(input)
+        if type(model) == str:
+            return show_model_alert(model)
+        try:
+            pred = model.predict(np.reshape(input, [1, -1]))
+        except:
+            volume = input[6] * input[7] * input[8]
+            input.append(volume)
+            pred = model.predict(np.reshape(input, [1, -1]))
+        return render_template('res.html',val=pred)
 
     
--- tests/test_routes.py
+++
@@ -1,5 +0,0 @@
-def test_route1():
-    assert 1==1
-
-
-    
